026857e8782e52800b6ab90a495ab94e43c3f4dc
Anne Mahaffey <anne.mahaffey@analog.com>
2012-07-26 15:13:47 -0400

    basic X read

--- ADXL362_RaspPi.c
+++ ADXL362_RaspPi.c
@@ -19,6 +19,43 @@ int main(int argc, char **argv)
     // buf will now be filled with the data that was read from the slave
     printf("Device ID: %02X \n", buf[2]);
     
+    char buf1[] = {0x0A, 0x1F, 0x52}; // Soft Reset
+    bcm2835_spi_transfern(buf1, sizeof(buf1));
+
+    delay(1000);
+
+    char buf2[] = {0x0A, 0x2D, 0x02}; // Setup for Measure
+    bcm2835_spi_transfern(buf2, sizeof(buf2));
+
+    delay(1000);
+
+    char buf3[] = {0x0B, 0x0E, 0x00, 0x00}; // Read X
+    bcm2835_spi_transfern(buf3, sizeof(buf3));
+    printf("X Axis is: %02X %02X \n", buf3[3], buf3[2]);
+
+    delay(1000);
+    printf("Device ID: %02X \n", buf[2]);
+    
+    char buf1[] = {0x0A, 0x1F, 0x52}; // Soft Reset
+    bcm2835_spi_transfern(buf1, sizeof(buf1));
+
+    delay(1000);
+
+    char buf2[] = {0x0A, 0x2D, 0x02}; // Setup for Measure
+    bcm2835_spi_transfern(buf2, sizeof(buf2));
+
+    delay(1000);
+
+    char buf3[] = {0x0B, 0x0E, 0x00, 0x00}; // Read X
+    bcm2835_spi_transfern(buf3, sizeof(buf3));
+    printf("X Axis is: %02X %02X \n", buf3[3], buf3[2]);
+
+    delay(1000);
+
+    char buf4[] = {0x0B, 0x0E, 0x00, 0x00}; // Read X
+    bcm2835_spi_transfern(buf4, sizeof(buf4));
+    printf("X Axis is: %02X %02X \n", buf4[3], buf4[2]);
+
     bcm2835_spi_end();
     return 0;
 }
